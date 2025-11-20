0fb2cc5386306385955713661f4751a25ee2e120
roma <r.shulai@yandex.ru>
2025-11-20 23:14:34 +0300

    Refactor

--- src/Paths.py
+++ src/Paths.py
@@ -2,8 +2,7 @@ import os
 
 # work_directory = "" - указываем свою  рабочую директорию
 # По умолчанию: домашний каталог
-# work_directory = os.path.expanduser("~") + "/"
-work_directory = "/home/roma/Study/"
+work_directory = os.path.expanduser("~") + "/"
 
 # массив с названиями предметов (как называются папки)
 subjects = ["OPD", "Inf", "Prog"]
@@ -15,5 +14,4 @@ subjects_tables = {x: work_directory+x+"/"+x.lower()+".md" for x in subjects}
 readme_path = work_directory + "README.md"
 
 # шаблон (по умолчанию должен быть в рабочей директории)
-# outline_path = work_directory + "Outline.md"
-outline_path = "/home/roma/Projects/readme-collector/src/Outline.md"
+outline_path = work_directory + "Outline.md"
